ccbfe760ca7bf924e10a8e07df243388d355ffd7
Sujith S <sujithsree009@gmail.com>
2025-04-19 12:45:06 +0530

    b commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 import SearchIcon from './search.svg'
 
-const API_URL='http://www.omdbapi.com?apikey=c032e2d7';
+const API_URL='https://www.omdbapi.com?apikey=c032e2d7';
 const movie1=
     {
         "Title": "Spiderman",
